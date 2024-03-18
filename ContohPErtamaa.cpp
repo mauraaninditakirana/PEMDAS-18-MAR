0dcf05724d6d0b99a6cdce704aac39852d3d1c7a
mauraaninditakirana <kiranamaura351@gmail.com>
2024-03-18 14:55:25 +0700

    lopping menggunakan for

---
+++ ContohPErtamaa.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[5];
+    for (i = 0; i < 5; i++)
+    {
+        cout << i << " : " << "Maura" << endl;
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Masukkan Nama = ";
+        cin >> arr[i];
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Nama ke - " << i << arr[i] << endl;
+    }
+}
